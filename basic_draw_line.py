dc2c4f89bf8f2e15ae7cff739cbf7213553a6fde
JoeNajowick <joseph.nyder@siu.edu>
2022-11-29 21:01:24 -0800

    base version

--- basic_draw_line.py
+++ basic_draw_line.py
@@ -1,6 +1,7 @@
 import turtle
 
 James = turtle.Turtle()  # instatiates object so it can exist name of object is James
+James.shape("turtle")  # changes the shape of the turtle
 James.color("Green")  # sets object color to red so we can keep track of it
-James.forward(160)
+James.forward(300)
 turtle.done()
--- change_to_turtle.py
+++ change_to_turtle.py
@@ -1,6 +1,6 @@
 import turtle
 
 James = turtle.Turtle()  # instatiates object so it can exist name of object is James
-James.shape("turtle")  # sets object color to red so we can keep track of it
-
+James.shape("turtle")  # changes the shape of the turtle
+James.color("Green")
 turtle.done()
