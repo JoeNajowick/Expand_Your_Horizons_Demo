4ca362b7d53343336e54e7c031375a32c021fc7d
JoeNajowick <joseph.nyder@siu.edu>
2022-11-30 12:28:10 -0800

    base version

--- basic_change_direction.py
+++ basic_change_direction.py
@@ -1,22 +1,9 @@
 import turtle
 
 James = turtle.Turtle()  # instatiates object so it can exist name of object is James
-
 James.shape("turtle")  # changes the shape of the turtle
 James.color("Green")  # sets object color to red so we can keep track of it
-
-James.lt(90)
-James.forward(100)
-James.lt(90)
-James.forward(100)
-James.lt(90)
-James.forward(100)
-James.lt(90)
-James.forward(100)
-James.hideturtle() #can move this up to not show the turtle during creation
-
+#James.lt(90)
+James.rt(90)
+James.forward(300)
 turtle.done()
-return()
-
-
-
