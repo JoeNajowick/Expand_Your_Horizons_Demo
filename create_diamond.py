fe1b22d0ee263920af3f2a52753aa6acc20c3550
JoeNajowick <joseph.nyder@siu.edu>
2022-12-01 21:14:32 -0800

    base version

---
+++ create_diamond.py
@@ -0,0 +1,22 @@
+import turtle
+
+James = turtle.Turtle()  # instatiates object so it can exist name of object is James
+
+James.shape("turtle")  # changes the shape of the turtle
+James.color("Green")  # sets object color to red so we can keep track of it
+
+James.lt(45)
+James.forward(100)
+James.lt(90)
+James.forward(100)
+James.lt(90)
+James.forward(100)
+James.lt(90)
+James.forward(100)
+James.hideturtle() #can move this up to not show the turtle during creation
+
+turtle.done()
+
+
+
+
