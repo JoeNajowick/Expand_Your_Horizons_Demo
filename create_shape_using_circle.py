40c41a127048d88334fe4f7ff25f38dc5224cf69
JoeNajowick <joseph.nyder@siu.edu>
2022-12-04 15:07:54 -0800

    base version

---
+++ create_shape_using_circle.py
@@ -0,0 +1,9 @@
+import turtle
+
+James = turtle.Turtle()  # instatiates object so it can exist name of object is James
+
+James.shape("turtle")  # changes the shape of the turtle
+James.color("Green")  # sets object color to red so we can keep track of it
+
+James.circle(80,steps= 4)
+turtle.done()
