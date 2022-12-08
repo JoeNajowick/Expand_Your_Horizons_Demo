758a04337924ae413aa14489424e2943db573003
JoeNajowick <joseph.nyder@siu.edu>
2022-12-07 21:31:49 -0800

    rbg color added

--- set_turtle_color.py
+++ set_turtle_color.py
@@ -2,5 +2,6 @@ import turtle
 
 James = turtle.Turtle()  # instatiates object so it can exist name of object is James
 James.color("Green")  # sets object color to red so we can keep track of it
-
+#turtle.colormode(255)
+#James.color((200,255,255))
 turtle.done()
