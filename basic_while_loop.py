b14efa35d00457f497966e052b0f25d9defbafea
JoeNajowick <joseph.nyder@siu.edu>
2022-12-08 16:43:29 -0800

    rbg color added

---
+++ basic_while_loop.py
@@ -0,0 +1,13 @@
+
+
+localcounter = 0
+while localcounter <= 10:
+    if localcounter % 2 == 1 :
+        print("its odd")
+    else:
+        print("its even")
+
+
+
+
+localcounter = localcounter + 1
--- set_turtle_color.py
+++ set_turtle_color.py
@@ -1,7 +1,7 @@
 import turtle
 
 James = turtle.Turtle()  # instatiates object so it can exist name of object is James
-James.color("Green")  # sets object color to red so we can keep track of it
-#turtle.colormode(255)
-#James.color((200,255,255))
+#James.color("Green")  # sets object color to red so we can keep track of it
+turtle.colormode(255)
+James.color((200,255,255))
 turtle.done()
