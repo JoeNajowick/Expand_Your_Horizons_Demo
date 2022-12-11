d41191d0b262741b2e3a82d9b339de2bdded7fd7
JoeNajowick <joseph.nyder@siu.edu>
2022-12-11 14:12:58 -0800

    notes added

--- basic_while_loop.py
+++ basic_while_loop.py
@@ -2,7 +2,7 @@
 
 localcounter = 0
 while localcounter <= 10:
-    if localcounter % 2 == 1 :
+    if localcounter % 2 == 1 : #if the counter variable is odd when you see a % in programmming it checks to see if there is a remainder after division
         print("its odd")
     else:
         print("its even")
