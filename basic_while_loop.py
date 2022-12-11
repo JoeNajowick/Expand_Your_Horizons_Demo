976c43cb4cefdd75348adfa6ebf0552490e9d9a8
JoeNajowick <joseph.nyder@siu.edu>
2022-12-11 14:14:07 -0800

    fixed infinite loop

--- basic_while_loop.py
+++ basic_while_loop.py
@@ -4,10 +4,7 @@ localcounter = 0
 while localcounter <= 10:
     if localcounter % 2 == 1 : #if the counter variable is odd when you see a % in programmming it checks to see if there is a remainder after division
         print("its odd")
+        localcounter = localcounter + 1
     else:
         print("its even")
-
-
-
-
-localcounter = localcounter + 1
+        localcounter = localcounter + 1
